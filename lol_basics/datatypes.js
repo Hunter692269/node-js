bebced78655d89eacab3be8156c70dab95f06512
Rushikesh Dighole <78699657+Hunter692269@users.noreply.github.com>
2024-05-23 09:28:05 +0000

    datatypes and variabkles

---
+++ lol_basics/datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// alert(3+3)  
+
+console.log(typeof null);// object
+console.log(typeof undefined);//undefined
---
+++ lol_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "rushi@gmail.com"
+var accountPassword = "Rushi@2002"
+accountCity = "Nashik"
+
+// accountId = 23
+accountEmail = "h.gamil.com"
+accountPassword = "23"
+accountCity = "No"
+let accountState;
+
+// console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
