b9fd777569b0cc8f5812be5b5ba682cef1293e7b
Rushikesh Dighole <78699657+Hunter692269@users.noreply.github.com>
2024-05-23 10:06:39 +0000

    conversions of datatype

---
+++ lol_basics/conversions.js
@@ -0,0 +1,19 @@
+let score ="true"
+
+console.log(typeof score);
+
+let val = Number(score)
+console.log(typeof val);
+console.log(val);
+
+// "22" => 22
+// "22abc" => NaN
+// true => 1 ; false => 0
+//null=>0
+
+let number = ""
+let bnum = Boolean(number)
+console.log(bnum);
+
+// 0=> false 1=> true
+// "" => false "a"=> true 
