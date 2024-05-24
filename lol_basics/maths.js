015aded996cf20b1c0895732862963a5454e4d52
Rushikesh Dighole <78699657+Hunter692269@users.noreply.github.com>
2024-05-24 06:20:53 +0000

    strings and math

---
+++ lol_basics/maths.js
@@ -0,0 +1,27 @@
+const num =100
+// console.log(num);
+
+const num2  = new Number(100);
+// console.log(num2);
+
+// console.log(num2.toString().length);
+// console.log(num2.toFixed(2));
+
+const num3 = 323.666
+// console.log(num3.toPrecision(4));
+// console.log(num3.toPrecision(3));
+
+const hun = 1000000
+// console.log(hun.toLocaleString());
+// console.log(hun.toLocaleString('en-IN'));
+
+// MATHS
+console.log(Math);
+
+console.log(Math.random());
+
+const max =20
+const min =10
+
+//for values between 10 and 20
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
---
+++ lol_basics/strings.js
@@ -0,0 +1,15 @@
+const name ="Rushi"
+const repoCount =50
+
+// console.log(name+repoCount+" Value"); Not a good practice
+
+// string intercolation
+console.log(`MY name is ${name} and my repo count is ${repoCount}`);
+
+const str1 = new String("Rushi-HC");
+// console.log(str1.toUpperCase()); such usual methods
+
+const str2 = str1.slice(0,4)
+console.log(str2);
+const str3 = str1.slice(-6,4)
+console.log(str3);
