1395c467dac00e1e91e68e8036ed195ebcf39418
Rushikesh Dighole <78699657+Hunter692269@users.noreply.github.com>
2024-05-26 09:08:37 +0000

    functions 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,4 +27,33 @@ function logIn(username = "man"){
     return `${username} just logged in`
 }
 // console.log(logIn("hello"))
-console.log(logIn());
+// console.log(logIn());
+
+
+
+function calcPrice(...num3){  //using REST operator here creates an array of all the passed values
+    return num3;
+}
+function calcPrice2(val1,val2,...num3){  //so 1st 2 values in val1 and val2 and rest in num1 array
+    return num3;
+}
+// console.log(calcPrice(200,300,400));
+// console.log(calcPrice2(200,300,400,300,450));
+
+const user = {
+    username:"hi",
+    price:100
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+
+
+const myArray =[200,300,400]
+
+function returnValue(getArray){
+    return getArray[1]
+}
+console.log(returnValue(myArray));
